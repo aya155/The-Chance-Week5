30439bc061e264231e844cd6227a17f22723849c
Aya-AlNaqeep <ayaalnaqeep@gmail.com>
2022-06-27 21:04:22 +0300

    initial commit

---
+++ app/src/main/java/com/aya_m_abed/week5_the_chance/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aya_m_abed.week5_the_chance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
